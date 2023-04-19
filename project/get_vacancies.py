133184fdb6f492f8140c8ab6b6f47d1e78ae58d1
RasimAbasov <abasovrasim.k@gmail.com>
2023-04-19 21:56:46 +0300

    Добавлена выгрузка по фильтру с датой, чтобы избежать ошибки с максимумом 2000 вакансий

--- project/get_vacancies.py
+++ project/get_vacancies.py
@@ -1,3 +1,21 @@
+import datetime
+
 from project.helpers.hh_loader import HHLoader
 
-HHLoader().get_all_vacancies()
+
+# https://github.com/hhru/api/blob/master/docs/general.md
+
+
+loader = HHLoader()
+
+start_time = datetime.datetime.now() - datetime.timedelta(days=30)
+end_time = datetime.datetime.now()
+
+while start_time <= end_time:
+    interval_end = start_time + datetime.timedelta(hours=1)
+    start_time_iso = start_time.isoformat(timespec='seconds')
+    interval_end_iso = interval_end.isoformat(timespec='seconds')
+    print(f"startTimeIso: {start_time_iso}, intervalEndIso: {interval_end_iso}")
+    loader.get_all_vacancies(date_from=start_time_iso, date_to=interval_end_iso)
+    start_time = interval_end
+
--- project/helpers/hh_loader.py
+++ project/helpers/hh_loader.py
@@ -1,7 +1,6 @@
 import json
 import math
 import os
-import time
 from http import HTTPStatus
 
 import requests
@@ -29,7 +28,7 @@ class HHLoader:
             f"Ожидаемый статус ответа: {HTTPStatus.OK}, фактический: {response.status_code}"
         return response.json()
 
-    def get_page(self, page: int = 0) -> dict:
+    def get_page(self, date_from, date_to, page: int = 0) -> dict:
         """
         Метод для получения страницы со списком вакансий
         Returns:
@@ -40,34 +39,34 @@ class HHLoader:
             "text": self.text_filter,
             "area": self.area,
             "page": page,
-            "per_page": self.count_vacancies_on_page
+            "per_page": self.count_vacancies_on_page,
+            "date_from": date_from,
+            "date_to": date_to
         }
-
         response = requests.get(url=Urls.vacancies.value, params=params)
         assert response.status_code == HTTPStatus.OK, \
             f"Ожидаемый статус ответа: {HTTPStatus.OK}, фактический: {response.status_code}, response: {response.text}"
         response.close()
         return response.json()
 
-    def get_all_found(self) -> int:
+    def get_all_found(self, date_from, date_to) -> int:
         """
 
         Returns:
 
         """
-        return self.get_page()['found']
+        return self.get_page(date_from=date_from, date_to=date_to)['found']
 
-    def get_all_vacancies(self):
-        all_vacancies = self.get_all_found()
+    def get_all_vacancies(self, date_from, date_to):
+        all_vacancies = self.get_all_found(date_from=date_from, date_to=date_to)
         count_pages = math.ceil(all_vacancies / self.count_vacancies_on_page)
         for page in range(0, count_pages + 1):
-            data = self.get_page(page=page)
-            path = os.path.normpath(os.path.join(os.getcwd(), "docs", "pagination", f"page_{page}.json"))
-            print(page)
-            # Создаем новый документ, записываем в него ответ запроса, после закрываем
-            f = open(path, mode='w', encoding='utf8')
-            f.write(json.dumps(data, ensure_ascii=False))
-            f.close()
-
-            # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим. 5 сек мы может подождать
-            time.sleep(5)
+            data = self.get_page(page=page, date_from=date_from, date_to=date_to)
+            if data['items'] is not None:
+                path = os.path.normpath(os.path.join(os.getcwd(), "docs", "pagination",
+                                                     f"page_{page}_{date_from.replace(':', '-')}.json"))
+                print(f'page_{page}_{date_from}-{date_to}')
+                # Создаем новый документ, записываем в него ответ запроса, после закрываем
+                f = open(path, mode='w', encoding='utf8')
+                f.write(json.dumps(data, ensure_ascii=False))
+                f.close()
